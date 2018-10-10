d33fdf4787689cdd74d6153d4952fc52bb2decdc
Sherwin Yu <sherwinhyu@gmail.com>
2018-10-10 01:30:01 -0400

    udpdate 404 msg

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,17 @@ const App = () => (
       <Switch>
         <Route exact path="/" />
         <Route path="/contact" component={Contact} />
-        <Route path="*" component={() => <h1 style={{ padding: '1em' }}>404: Not Found</h1>} />
+        <Route
+          path="*"
+          component={() => (
+            <h1 style={{ textAlign: 'center', padding: '1em' }}>
+              404: Page Not Found
+              <span role="img" aria-label="confounded-face" style={{ marginLeft: '5px' }}>
+                😖
+              </span>
+            </h1>
+          )}
+        />
       </Switch>
     </BrowserRouter>
   </div>
