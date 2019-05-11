80b105cde8794f45a883b8098053c3723fdb232c
Sherwin Yu <sherwinhyu@gmail.com>
2019-05-11 14:19:23 -0400

    remove unused package and styled social links

--- src/components/common/SocialLinks.js
+++ src/components/common/SocialLinks.js
@@ -3,26 +3,46 @@ import styled from 'styled-components';
 import { faGithub, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const Icon = styled.div`
-  font-size: 28px;
+const FlexWrapper = styled.div`
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  margin-left: 15px;
+  margin-top: 75px;
+`;
+
+const ListItem = styled.li`
+  font-size: 32px;
   margin-bottom: 15px;
-  &:hover {
-    cursor: pointer;
+  a {
+    color: black;
+    &:hover {
+      cursor: pointer;
+      color: #0062ff;
+    }
   }
 `;
 
 const SocialLinks = () => (
-  <div style={{ marginTop: '100px' }}>
-    <Icon>
-      <FontAwesomeIcon icon={faGithub} />
-    </Icon>
-    <Icon>
-      <FontAwesomeIcon icon={faLinkedin} />
-    </Icon>
-    <Icon>
-      <FontAwesomeIcon icon={faInstagram} />
-    </Icon>
-  </div>
+  <FlexWrapper>
+    <ul>
+      <ListItem>
+        <a href="https://github.com/sherwin-yu">
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+      </ListItem>
+      <ListItem>
+        <a href="https://www.linkedin.com/in/sherwinhyu">
+          <FontAwesomeIcon icon={faLinkedin} />
+        </a>
+      </ListItem>
+      <ListItem>
+        <a href="https://www.instagram.com/sherwin.yu">
+          <FontAwesomeIcon icon={faInstagram} />
+        </a>
+      </ListItem>
+    </ul>
+  </FlexWrapper>
 );
 
 export default SocialLinks;
