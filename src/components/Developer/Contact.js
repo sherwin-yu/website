75fc1dbd91de108c498439ca773d3a92fa4e566b
Sherwin Yu <sherwinhyu@gmail.com>
2019-10-03 00:37:55 -0400

    simplify contact section

--- src/components/Developer/Contact.js
+++ src/components/Developer/Contact.js
@@ -1,23 +1,17 @@
 import React from 'react';
-import { TextInput, TextArea, Button } from 'carbon-components-react';
+import { Button } from 'carbon-components-react';
 import styled from 'styled-components';
 
 const Section = styled.section`
   font-size: 20px;
-  height: 75vh;
+  height: 50vh;
   background-color: #eae9e9;
   color: black;
   display: flex;
   align-items: center;
+  text-align: center;
 `;
 
-const InputWrapper = styled.div`
-  margin-bottom: 15px;
-`;
-
-const ButtonWrapper = styled.div`
-  margin-bottom: 15px;
-`;
 const SectionHeader = styled.div`
   font-weight: bold;
   font-size: 32px;
@@ -25,35 +19,33 @@ const SectionHeader = styled.div`
   color: #420000;
 `;
 
-const Contact = ({ handleChange, handleSubmit }) => (
+const ContactMessage = styled.p`
+  margin: 50px 0px;
+`;
+
+const LinkButton = styled.a`
+  text-decoration: none;
+`;
+
+const ButtonWrapper = styled.div`
+  text-align: center;
+`;
+
+const Contact = () => (
   <Section>
     <div className="col-lg-6 offset-lg-3">
-      <SectionHeader>Contact</SectionHeader>
-
-      <div className="col-lg-8 offset-lg-2">
-        <form onSubmit={handleSubmit}>
-          <InputWrapper>
-            <TextInput type="text" name="name" labelText="Name" onChange={handleChange} required />
-          </InputWrapper>
-          <InputWrapper>
-            <TextInput type="email" name="email" labelText="Email" onChange={handleChange} required />
-          </InputWrapper>
-          <InputWrapper>
-            <TextInput type="text" name="subject" labelText="Subject" onChange={handleChange} required />
-          </InputWrapper>
-          <InputWrapper>
-            <TextArea name="message" labelText="Message" rows={5} onChange={handleChange} required />
-          </InputWrapper>
-          <ButtonWrapper>
-            <Button type="submit" style={{ backgroundColor: '#408bfc' }}>
-              <div style={{ fontWeight: 'bold', fontSize: '16px' }}>SEND MESSAGE</div>
-            </Button>
-          </ButtonWrapper>
-        </form>
-        <Button type="button" style={{ backgroundColor: '#408bfc' }}>
-          <div style={{ fontWeight: 'bold', fontSize: '16px' }}>CONTACT</div>
-        </Button>
-      </div>
+      <SectionHeader>What&#39;s Next?</SectionHeader>
+      <ContactMessage>
+        My inbox is always open. Feel free to send me a message to get <br /> in touch, and I&#39;ll do my best to
+        answer your email.
+      </ContactMessage>
+      <ButtonWrapper>
+        <LinkButton href="mailto:sherwinhyu@gmail.com">
+          <Button type="button" style={{ backgroundColor: '#408bfc' }}>
+            <div style={{ fontWeight: 'bold', fontSize: '16px' }}>CONTACT</div>
+          </Button>
+        </LinkButton>
+      </ButtonWrapper>
     </div>
   </Section>
 );
--- src/components/Developer/DeveloperContainer.js
+++ src/components/Developer/DeveloperContainer.js
@@ -1,64 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Intro from './Intro';
 import About from './About';
 import Experience from './Experience';
 import Contact from './Contact';
 
-class DeveloperContainer extends Component {
-  constructor(props, context) {
-    super(props, context);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.state = {
-      name: '',
-      email: '',
-      subject: '',
-      message: ''
-    };
-  }
-
-  handleChange(event) {
-    event.preventDefault();
-    const { name, value } = event.target;
-    this.setState({ [name]: value }); // eslint-disable-line react/no-unused-state
-  }
-
-  handleSubmit(event) {
-    event.preventDefault();
-    // eslint-disable-next-line no-console
-    console.log('SUBMITING', this.state);
-    const body = this.state;
-    fetch('/api/mail', {
-      headers: { 'Content-Type': 'application/json' },
-      method: 'POST',
-      credentials: 'include',
-      body: JSON.stringify(body)
-    })
-      .then(response => {
-        this.state = {};
-        return response.json();
-      })
-      .catch(err => err);
-  }
-
-  render() {
-    const { name, email, subject, message } = this.state;
-    return (
-      <>
-        <Intro />
-        <About />
-        <Experience />
-        <Contact
-          name={name}
-          email={email}
-          subject={subject}
-          message={message}
-          handleChange={this.handleChange}
-          handleSubmit={this.handleSubmit}
-        />
-      </>
-    );
-  }
-}
+const DeveloperContainer = () => (
+  <>
+    <Intro />
+    <About />
+    <Experience />
+    <Contact />
+  </>
+);
 
 export default DeveloperContainer;
