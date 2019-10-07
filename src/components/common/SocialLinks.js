dc6a375adb57b383578201b8b89a504671e3db6f
Sherwin Yu <sherwinhyu@gmail.com>
2019-10-07 00:22:34 -0400

    adjust position of social links

--- src/components/common/SocialLinks.js
+++ src/components/common/SocialLinks.js
@@ -3,46 +3,39 @@ import styled from 'styled-components';
 import { faGithub, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const FlexWrapper = styled.div`
-  position: fixed;
-  display: flex;
-  justify-content: center;
+const Wrapper = styled.div`
+  position: absolute;
   margin-left: 15px;
-  margin-top: 75px;
+  margin-top: 15px;
+  z-index: 999;
+  float: right;
+  right: 0;
+  top: 0;
 `;
 
-const ListItem = styled.li`
-  font-size: 32px;
-  margin-bottom: 15px;
-  a {
+const SocialItem = styled.a`
+  font-size: 40px;
+  margin-right: 15px;
+  cursor: pointer;
+  color: #420000;
+  &:hover {
+    cursor: pointer;
     color: #420000;
-    &:hover {
-      cursor: pointer;
-      color: #0062ff;
-    }
   }
 `;
 
 const SocialLinks = () => (
-  <FlexWrapper>
-    <ul>
-      <ListItem>
-        <a href="https://github.com/sherwin-yu">
-          <FontAwesomeIcon icon={faGithub} />
-        </a>
-      </ListItem>
-      <ListItem>
-        <a href="https://www.linkedin.com/in/sherwinhyu">
-          <FontAwesomeIcon icon={faLinkedin} />
-        </a>
-      </ListItem>
-      <ListItem>
-        <a href="https://www.instagram.com/sherwin.yu">
-          <FontAwesomeIcon icon={faInstagram} />
-        </a>
-      </ListItem>
-    </ul>
-  </FlexWrapper>
+  <Wrapper>
+    <SocialItem href="https://github.com/sherwin-yu">
+      <FontAwesomeIcon icon={faGithub} />
+    </SocialItem>
+    <SocialItem href="https://www.linkedin.com/in/sherwinhyu">
+      <FontAwesomeIcon icon={faLinkedin} />
+    </SocialItem>
+    <SocialItem href="https://www.instagram.com/sherwin.yu">
+      <FontAwesomeIcon icon={faInstagram} />
+    </SocialItem>
+  </Wrapper>
 );
 
 export default SocialLinks;
