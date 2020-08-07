f9edf3a2a58cff6340e23b06398b0995d865ea49
Sherwin Yu <sherwinhyu@gmail.com>
2020-08-06 23:11:15 -0400

    add projects

--- src/components/Developer/Projects.js
+++ src/components/Developer/Projects.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLink } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import spotifyOverview from '../../assets/spotifyOverview.png';
 
 const Section = styled.section`
   font-size: 20px;
@@ -21,25 +25,82 @@ const SectionHeader = styled.div`
 const ProjectTitle = styled.div`
   font-size: 20px;
   font-weight: 600;
+  margin-bottom: 1rem;
+`;
+
+const ProjectDescription = styled.div`
+  margin-bottom: 1rem;
 `;
 
 const StyledList = styled.ul`
-  padding-left: 40px;
   list-style-type: none;
+`;
+
+const Tag = styled.div`
+  font-size: 1rem;
+  font-weight: 400;
+  line-height: 1rem;
+  letter-spacing: 0.32px;
+  display: inline-flex;
+  align-items: center;
+  padding: 0 0.7rem;
+  height: 2rem;
+  margin: 0.5rem;
+  border-radius: 0.9375rem;
+  color: white;
+  background-color: #457b9d;
+`;
+
+const WebPreview = styled.img`
+  width: 325px;
+`;
+
+const LinkWrapper = styled.div`
+  margin-top: 1rem;
+`;
+
+const ExternalLink = styled.a`
+  font-size: 32px;
+  margin-right: 15px;
+  cursor: pointer;
+  color: #171717;
+  &:hover {
+    cursor: pointer;
+    color: lightgray;
+  }
 `;
 
 const Projects = () => (
   <Section>
     <div className="col-xl-6 offset-xl-3 col-lg-8 offset-lg-2 col-md-8 offset-md-2 col-sm-10 offset-sm-1 col-xs-12">
       <SectionHeader>Projects</SectionHeader>
-      <ProjectTitle>Spotify Overview</ProjectTitle>
-      <div>A web application visualizing an overview of your Spotify profile.</div>
-      <StyledList>
-        <li>React</li>
-        <li>Node.js</li>
-        <li>Express</li>
-        <li>Spotify Web API</li>
-      </StyledList>
+      <div className="row">
+        <div className="col-md-8">
+          <ProjectTitle>Spotify Overview</ProjectTitle>
+          <ProjectDescription>A web application visualizing an overview of your Spotify profile.</ProjectDescription>
+          <StyledList>
+            <Tag>React</Tag>
+            <Tag>Node.js</Tag>
+            <Tag>Express</Tag>
+            <Tag>Spotify Web API</Tag>
+          </StyledList>
+          <LinkWrapper>
+            <ExternalLink
+              href="https://github.com/sherwin-yu/spotify-overview"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              <FontAwesomeIcon icon={faGithub} />
+            </ExternalLink>
+            <ExternalLink href="https://spotifyoverview.herokuapp.com" target="_blank" rel="noreferrer noopener">
+              <FontAwesomeIcon icon={faLink} />
+            </ExternalLink>
+          </LinkWrapper>
+        </div>
+        <div className="col-md-4">
+          <WebPreview src={spotifyOverview} alt="spotifyoverview" />
+        </div>
+      </div>
     </div>
   </Section>
 );
